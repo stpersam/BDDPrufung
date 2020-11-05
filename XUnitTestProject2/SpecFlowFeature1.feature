6a85e2b5d70ff2e2ba4a121e020802072ea4f1d0
Samuel <samuel.pernthaler@gmail.com>
2020-11-05 10:37:40 +0100

    Add project files.

---
+++ XUnitTestProject2/SpecFlowFeature1.feature
@@ -0,0 +1,23 @@
+﻿Feature: SpecFlowFeature1
+    In order to avoid silly mistakes
+    As a math idiot
+    I want to be told the sinus/cosinus/tanges of numbers
+
+@mytag
+Scenario: Sinus of a number
+    Given the first number is 3.1415
+    When the sinus is calculated
+    Then the result should be 0
+
+
+Scenario: Kosinus of a number
+    Given the first number is 3.1415
+    When the cosinus is calculated
+    Then the result should be -1
+
+
+Scenario: Tangens of a number
+    Given the first number is 3.1415
+    When the tangens is calculated
+    Then the result should be 0
+    |
---
+++ XUnitTestProject2/SpecFlowFeature1Steps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace XUnitTestProject2
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double pi;
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(double p0)
+        {
+            pi = p0;
+        }
+        
+        [When(@"the sinus is calculated")]
+        public void WhenTheSinusIsCalculated()
+        {
+            Math.Sin(pi);
+        }
+        
+        [When(@"the cosinus is calculated")]
+        public void WhenTheCosinusIsCalculated()
+        {
+            Math.Cos(pi);
+        }
+        
+        [When(@"the tangens is calculated")]
+        public void WhenTheTangensIsCalculated()
+        {
+            Math.Tan(pi);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = pi;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
